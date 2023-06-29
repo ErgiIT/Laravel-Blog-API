f19f74ed3c0d0dde73ee59e06ca26d0ab73c2758
Ergi Aliko <ergialiko.it@gmail.com>
2023-06-29 09:33:49 +0200

    Posts CRUD, login and register done

---
+++ app/Http/Resources/PostsResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => (string)$this->id,
+            "attributs" => [
+                "title" => $this->title,
+                "desc" => $this->desc,
+                "public" => $this->public,
+                "created_at" => $this->created_at,
+                "updated_at" => $this->updated_at
+            ],
+            "relationships" => [
+                "id" => (string)$this->user->id,
+                "user name" => $this->user->name,
+                "user email" => $this->user->email
+            ]
+        ];
+    }
+}
