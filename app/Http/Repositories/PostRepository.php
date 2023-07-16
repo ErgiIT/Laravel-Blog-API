26129ed608d206572861d47b3f88b1a7f7c32c98
Ergi Aliko <ergialiko.it@gmail.com>
2023-07-16 13:47:56 +0200

    Added Post Service

--- app/Http/Repositories/PostRepository.php
+++ app/Http/Repositories/PostRepository.php
@@ -39,50 +39,6 @@ class PostRepository
 
     public function show($id)
     {
-        $loggedInUserId = Auth::user()->id;
-
-        $post = Post::where('id', $id)
-            ->where(function ($query) use ($loggedInUserId) {
-                $query->where('user_id', $loggedInUserId)
-                    ->orWhere("public", true)
-                    ->orWhereHas('shares', function ($query) use ($loggedInUserId) {
-                        $query->where('user_id', $loggedInUserId);
-                    });
-            })
-            ->first();
-
-        if ($post) {
-            $categoryNames = $post->categories->pluck('name');
-
-            $similarPosts = Post::where(function ($query) use ($categoryNames) {
-                $query->where('public', true)
-                    ->orWhereHas('shares', function ($query) {
-                        $query->where('user_id', Auth::user()->id);
-                    });
-            })
-                ->whereHas('categories', function ($query) use ($categoryNames) {
-                    $query->whereIn('name', $categoryNames);
-                })
-                ->where('id', '!=', $post->id)
-                ->get();
-
-
-
-            $rankedPosts = $similarPosts->map(function ($similarPost) use ($categoryNames) {
-                $similarPostCategories = $similarPost->categories->pluck('name');
-                $commonCategories = $categoryNames->intersect($similarPostCategories);
-                $count = $commonCategories->count();
-
-                return [
-                    'post' => new PostsResource($similarPost),
-                    'category_count' => $count,
-                ];
-            })->sortByDesc('category_count')->values();
-
-            $data = [
-                'post' => new PostsResource($post),
-                'similar_posts' => $rankedPosts,
-            ];
         $data = $this->postService->getSimilarAndRankedPosts($id);
 
         if ($data) {
